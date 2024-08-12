99203c0e7426b33738d755f587ad6384feba9425
Felipe Ortiz <felort15@gmail.com>
2024-08-11 20:20:12 -0600

    Rest

--- src/05-rest-spread.js
+++ src/05-rest-spread.js
@@ -7,7 +7,27 @@ console.log(a,b);
 //output: apple Banana
 
 //Objet destruturing
-let userName ={userName:"oscar", age:34};
-let {userName: name, age} = userName;
-console.log(userName, age);
+let user ={userName:"oscar", age: 34};
+let {userName, name, age} = user;
+console.log(userName,user,age);
 //output: { userName: 'oscar', age: 34 } 
+
+//spread operator
+let person = {
+    name:'oscar',age:28,
+};
+let country = 'mx';
+ 
+let data = {id: 1,...person, country};
+console.log(data);
+
+//rest
+
+function sum (num, ...values){
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+sum(10, 5, 5, 5, 5, 5,)
+
+
