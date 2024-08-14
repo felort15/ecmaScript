aed4b1e50626488fcd9ac050821b9c73d35fb798
Felipe Ortiz <felort15@gmail.com>
2024-08-13 18:26:29 -0600

    ES-7

--- es-7.js
+++ es-7.js
@@ -1,3 +1,14 @@
 const data = 3 **4;
 console.log(data);
 // Output: 81
+
+//array includes
+let numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.includes(3));
+// Output: true
+
+const list = ['Oscar', 'Davis', 'Ana']
+
+console.log(list.includes('Ana'));
+//output = true
