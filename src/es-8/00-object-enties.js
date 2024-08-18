399689f328a865758d577ddc78bf9c477b6fd288
Felipe Ortiz <felort15@gmail.com>
2024-08-17 19:46:12 -0600

    String-padding

---
+++ src/es-8/00-object-enties.js
@@ -0,0 +1,9 @@
+//Entries
+const countries = {
+    MX: 'mexico', CO: 'Colombia', CL: 'Chile', PE:'Peru'
+}
+console.log(Object.entries(countries));
+
+//values 
+
+console.log(Object.values(countries));
---
+++ src/es-8/string-padding.js
@@ -0,0 +1,13 @@
+const string = 'Hello';
+
+console.log(string.padStart(5, 'hi'));
+//empezara arellenar desde el numero del log dependiendo del num sera las poscicionesque rellene
+
+
+//trailing comas 
+//es una forma de evitar errores de sintaxis en objetos y arrays
+
+const array = [24,25,26,28,,,,,,30]
+
+console.log(array);
+console.log(array.length);
