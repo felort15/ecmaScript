83b553967ae9a315c2f9e28502451b50ad928a5a
Felipe Ortiz <felort15@gmail.com>
2024-08-17 20:58:11 -0600

    Iteracion de asincronia

--- src/es-9/async-iteracion.js
+++ src/es-9/async-iteracion.js
@@ -8,3 +8,11 @@ other.next().then(Response=> console.log(Response.value));
 other.next().then(Response=> console.log(Response.value));
 other.next().then(Response=> console.log(Response.value));
 console.log('Hello');
+
+async function arrayOfNames(array) {
+    for await (let value of array){
+        console.log(value);
+    }  
+}
+const names = arrayOfNames(['Oscar', 'David', 'Ana'])
+console.log('After');
