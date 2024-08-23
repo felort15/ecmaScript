56b76d110a2eada13b1f380d168474fdd061ee0d
Felipe Ortiz <felort15@gmail.com>
2024-08-22 21:53:56 -0600

    Async ecma9

---
+++ src/es-9/Async.js
@@ -0,0 +1,21 @@
+async function* anothegenerator(params) {
+    yield await Promise.resolve(1);
+    yield await Promise.resolve(2);
+    yield await Promise.resolve(3);
+}
+
+const other = anothegenerator();
+other.next().then(response => console.log(response.value));
+other.next().then(response => console.log(response.value));
+other.next().then(response => console.log(response.value));
+console.log('Hello!');
+
+async function arrayOfNames (array) {
+    for await (let value of array)
+{
+    console.log(value);
+}
+}
+
+const names= arrayOfNames(['Oscar','David', 'Ana',]);
+console.log('Hello!')
